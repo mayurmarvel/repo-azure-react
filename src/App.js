d5672f8d6c4c93ae654c518e0f2532cfcf0b230a
Antonio <joseantoniosysnet@gmail.com>
2023-01-13 05:33:37 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Proyecto de Azure y React
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://learn.microsoft.com/es-es/azure/?product=popular"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprende Azure!
         </a>
       </header>
     </div>
